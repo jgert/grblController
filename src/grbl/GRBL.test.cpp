5e84a0bef383da8ed04c86929d8147645e18c08d
Jakub Gert <kubagert@gmail.com>
2020-06-10 00:51:26 +0200

    Added integration tests for status message

--- src/grbl/GRBL.test.cpp
+++ src/grbl/GRBL.test.cpp
@@ -9,9 +9,17 @@
 
 Q_DECLARE_METATYPE(State)
 
+Q_DECLARE_METATYPE(vec3)
+
+Q_DECLARE_METATYPE(AccessoryState)
+
+Q_DECLARE_METATYPE(BufferState)
+
+Q_DECLARE_METATYPE(InputPinState)
+
 Q_DECLARE_METATYPE(MachineState)
 
-Q_DECLARE_METATYPE(vec3)
+Q_DECLARE_METATYPE(OverriddenValues)
 
 class GRBLTests : public TestSuite {
 Q_OBJECT
@@ -19,9 +27,100 @@ Q_OBJECT
 private slots:
 
     void initTestCase() {
-        qRegisterMetaType<State>();
+        qRegisterMetaType<AccessoryState>();
+        qRegisterMetaType<BufferState>();
+        qRegisterMetaType<InputPinState>();
         qRegisterMetaType<MachineState>();
+        qRegisterMetaType<OverriddenValues>();
+        qRegisterMetaType<State>();
         qRegisterMetaType<vec3>();
+    }
+
+    void testIntegrationAccessoryState() {
+        GRBL controller;
+
+        QSignalSpy spy(&controller, &GRBL::onReceivedAccessoryState);
+
+        controller.parse("<A:SF>");
+
+        QCOMPARE(spy.count(), 1);
+        AccessoryState state = qvariant_cast<AccessoryState>(spy.takeFirst()[0]);
+        QCOMPARE(state.spindleState, AccessoryState::SpindleState::enabledCW);
+        QCOMPARE(state.floodCoolant, true);
+        QCOMPARE(state.mistCoolant, false);
+    }
+
+    void testIntegrationBufferState() {
+        GRBL controller;
+
+        QSignalSpy spy(&controller, &GRBL::onReceivedBufferState);
+
+        controller.parse("<Bf:120,10>");
+
+        QCOMPARE(spy.count(), 1);
+        auto state = qvariant_cast<BufferState>(spy.takeFirst()[0]);
+        QCOMPARE(state.availableBlocks, 120);
+        QCOMPARE(state.availableBytesInRxBuffer, 10);
+    }
+
+    void testIntegrationFeedRate() {
+        GRBL controller;
+
+        QSignalSpy spy(&controller, &GRBL::onReceivedFeedRate);
+
+        controller.parse("<F:120>");
+
+        QCOMPARE(spy.count(), 1);
+        auto state = qvariant_cast<unsigned int>(spy.takeFirst()[0]);
+        QCOMPARE(state, 120);
+    }
+
+    void testIntegrationFeedRateSpindleSpeed() {
+        GRBL controller;
+
+        QSignalSpy spy1(&controller, &GRBL::onReceivedFeedRate);
+        QSignalSpy spy2(&controller, &GRBL::onReceivedSpindleSpeed);
+
+        controller.parse("<FS:120,210>");
+
+        QCOMPARE(spy1.count(), 1);
+        auto state1 = qvariant_cast<unsigned int>(spy1.takeFirst()[0]);
+        QCOMPARE(state1, 120);
+
+        QCOMPARE(spy2.count(), 1);
+        auto state2 = qvariant_cast<unsigned int>(spy2.takeFirst()[0]);
+        QCOMPARE(state2, 210);
+    }
+
+    void testIntegrationInputPinState() {
+        GRBL controller;
+
+        QSignalSpy spy(&controller, &GRBL::onReceivedInputPinState);
+
+        controller.parse("<Pn:XZDHR>");
+
+        QCOMPARE(spy.count(), 1);
+        auto state = qvariant_cast<InputPinState>(spy.takeFirst()[0]);
+        QCOMPARE(state.xLimit, true);
+        QCOMPARE(state.yLimit, false);
+        QCOMPARE(state.zLimit, true);
+        QCOMPARE(state.probe, false);
+        QCOMPARE(state.door, true);
+        QCOMPARE(state.hold, true);
+        QCOMPARE(state.softReset, true);
+        QCOMPARE(state.cycleStart, false);
+    }
+
+    void testIntegrationLineNumber() {
+        GRBL controller;
+
+        QSignalSpy spy(&controller, &GRBL::onReceivedLineNumber);
+
+        controller.parse("<Ln:120>");
+
+        QCOMPARE(spy.count(), 1);
+        auto state = qvariant_cast<unsigned int>(spy.takeFirst()[0]);
+        QCOMPARE(state, 120);
     }
 
     void testIntegrationMachineStateValid() {
@@ -47,7 +146,21 @@ private slots:
         QCOMPARE(spy.count(), 0);
     }
 
-    void testIntegrationMachineStatePositionWPos() {
+    void testIntegrationOverriddenValues() {
+        GRBL controller;
+
+        QSignalSpy spy(&controller, &GRBL::onReceivedOverriddenValues);
+
+        controller.parse("<Ov:1,2,3>");
+
+        QCOMPARE(spy.count(), 1);
+        auto state = qvariant_cast<OverriddenValues>(spy.takeFirst()[0]);
+        QCOMPARE(state.feed, 1);
+        QCOMPARE(state.rapid, 2);
+        QCOMPARE(state.spindle, 3);
+    }
+
+    void testIntegrationWPos() {
         GRBL controller;
 
         QSignalSpy spyMPos(&controller, &GRBL::onReceivedMPos);
@@ -65,7 +178,7 @@ private slots:
         QCOMPARE(qvariant_cast<vec3>(spyWPos[1].takeFirst()), vec3(-40, -40, -40));
     }
 
-    void testIntegrationMachineStatePositionMPos() {
+    void testIntegrationMachineMPos() {
         GRBL controller;
 
         QSignalSpy spyMPos(&controller, &GRBL::onReceivedMPos);
